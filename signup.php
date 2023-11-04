9c86589a6a8a5c8316e45056c2e83f1a830d43b4
ypmk <kopylovaksusha123@gmail.com>
2023-11-04 20:13:49 +0300

    Добавлена станица signup

---
+++ signup.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyChattttt</title>
+    <link rel="stylesheet" href="style_signup.css">
+</head>
+<body>
+    <div id="wrapper_signup">
+       
+        <div id="header">
+            MyChat
+            <div style="font-size:20px; font-family:myFont;">Login</div>
+            <br>
+        </div>
+        <form>
+            <input type="text" name="username" placeholder="Username"><br>
+            <div style="padding:10px;">
+                <br>Gender:<br>
+                <input type="radio" name="gender">Male<br>
+                <input type="radio" name="gender">Female<br>
+            </div>
+            <input type="password" name="password" placeholder="Password"><br>
+            <input type="password" name="password2" placeholder="Retype Password"><br>
+            <input type="submit" value="Sigh up"><br>
+
+        </form>
+
+
+    </div>
+</body>
+</html>
