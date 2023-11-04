7eb95652df68160a9b151c0ea9cc14891e860ccf
ypmk <kopylovaksusha123@gmail.com>
2023-11-04 16:44:36 +0300

    Начато создание приложения

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyChattttt</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="left_pannel">
+            <div id="user_wrap">
+                <img src="ui/images/user3.jpg">
+                <br>
+                Kelly Hartmann
+                <br>
+                <span style="font-size:12px; opacity:0.5">kellyHartmen@gmail.com</span>
+            
+                <div>
+                    <label for="box">Chat</label><br>
+                    <label>Contacts</label>
+                    <label>Settings</label>
+                </div>
+            </div>
+        </div>
+        <div id="right_pannel">
+            <div id="header">MyChat</div>
+            <div id="container">
+
+                <div id="inner_left_pannel">
+                    <input type="checkbox" id="box" name = ""></input>
+                </div>
+                <div id ="inner_right_pannel"></div>
+
+            </div>
+        </div>
+
+
+
+    </div>
+</body>
+</html>
