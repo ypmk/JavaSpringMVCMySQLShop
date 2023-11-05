ffc4e51ff69d923569290485a3b04d529f1e3956
ypmk <kopylovaksusha123@gmail.com>
2023-11-05 13:51:52 +0300

    Добавлено получение введенных пользователем данных на странице signup по запросу

--- signup.php
+++ signup.php
@@ -11,23 +11,101 @@
        
         <div id="header">
             MyChat
-            <div style="font-size:20px; font-family:myFont;">Login</div>
+            <div style="font-size:20px; font-family:myFont;">Signup</div>
             <br>
         </div>
-        <form>
+        <form id = "myform">
             <input type="text" name="username" placeholder="Username"><br>
+            <input type="text" name="email" placeholder="Email"><br>
+
             <div style="padding:10px;">
                 <br>Gender:<br>
-                <input type="radio" name="gender">Male<br>
-                <input type="radio" name="gender">Female<br>
+                <input type="radio" value="Male" name="gender">Male<br>
+                <input type="radio" value="Female" name="gender">Female<br>
             </div>
+
             <input type="password" name="password" placeholder="Password"><br>
             <input type="password" name="password2" placeholder="Retype Password"><br>
-            <input type="submit" value="Sigh up"><br>
-
+            <input type="submit" value="Sigh up" id="signup_button"><br>
         </form>
 
 
     </div>
 </body>
 </html>
+
+
+<script type="text/javascript">
+
+    function _(element){
+        return document.getElementById(element);
+    }
+
+    var signup_button = _("signup_button");
+    signup_button.addEventListener("click", collect_data);
+
+    function collect_data(){
+        
+        var myform = _("myform");
+        var inputs = myform.getElementsByTagName("INPUT");
+
+        var data = {};
+        for (var i = inputs.length-1; i>=0; i--){
+
+            var key = inputs[i].name;
+
+            switch(key){
+
+                case "username":
+                    data.username = inputs[i].value;
+                    break;
+
+                case "email":
+                    data.email = inputs[i].value;
+                    break;
+
+                case "gender":
+                    if(inputs[i].checked){
+                        data.gender = inputs[i].value;
+                    }
+                    break;
+
+                case "password":
+                    data.password = inputs[i].value;
+                    break;
+
+                case "password2": 
+                    data.password2 = inputs[i].value;
+                    break;
+
+
+            }
+        }
+
+        send_data(data,"signup");
+
+    }
+
+
+    function send_data(data, type){
+
+        var xml = new XMLHttpRequest();
+
+        xml.onload = function(){
+
+            if(xml.readyState == 4 || xml.status == 200){
+
+                alert(xml.responseText);
+            }
+        }
+
+            data.data_type = type;
+            var data_string = JSON.stringify(data);
+
+            xml.open("POST", "api.php", true);
+            xml.send(data_string);
+        
+
+    }
+    
+</script>
